45dd9e788fa60879f39cb7562d8e7b7c04beed80
Devansh Desai <devanshdesai@hotmail.com>
2016-07-18 21:43:46 -0400

    Initial setup of snoocore

--- src/app.js
+++ src/app.js
@@ -1,4 +1,22 @@
-var ipc = require("electron").ipcRenderer;
+var ipc = require("electron").ipcRenderer,
+    Snoocore = require("snoocore");
+
+var reddit = new Snoocore({
+      userAgent: 'test@documentation',
+      oauth: {
+        type: 'implicit',
+        mobile: true,
+        key: 'tj19dKrZ6PG5UA',
+        redirectUri: 'http://localhost.com/',
+        scope: ["identity", "privatemessages"]
+      }
+  });
+  
+$(document).ready(function () {
+  
+
+});
+
 
 $("#quit").click(function() {
   ipc.send("quit");
