4af84de40cf3406c13dd1268023998625047493c
Devansh Desai <devanshdesai@hotmail.com>
2016-08-11 15:06:36 -0400

    Switched from jQuery with Vue.js

--- src/app.js
+++ src/app.js
@@ -7,32 +7,45 @@ persist.initSync({
     dir: process.resourcesPath + "/persist"
 });
 
-$(document).ready(function() {
+var main = new Vue({
+    el: "#main",
+    data: {
+        ready: false,
+        user: {
+            name: "null",
+            karma: {
+                link: "null",
+                comment: "null"
+            }
+        },
+        messages: [],
+        new_messages: false,
+        loading: false,
+        interval: 2,
+        first_check: true
+    },
+    methods: {
+        authenticate: function() {
+            reddit.authenticate(function(success) {
+                if (success) {
+        			reddit.getMe(function(user) {
+        				if (user.mail) {
+        					console.log("Mail!");
+        				}
+        				if (!user.mail) {
+        					console.log("No Mail!");
+        				}
+        			});
+                }
+            });
 
-});
-
-
-$("#quit").click(function() {
-    ipc.send("quit");
-});
-
-$("#minimize").click(function() {
-    ipc.send("minimize");
-});
-
-$("#authenticate").click(function() {
-    reddit.authenticate(function(success) {
-        if (success) {
-			reddit.getMe(function(user) {
-				if (user.mail) {
-					console.log("Mail!");
-				}
-				if (!user.mail) {
-					console.log("No Mail!");
-				}
-			});
+            console.log(module.exports);
+        },
+        minimizeApp: function() {
+            ipc.send("minimize");
+        },
+        quitApp: function() {
+            ipc.send("quit");
         }
-    });
-
-    console.log(exports);
+    }
 });
